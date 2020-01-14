36c39539fe06cd21e42eae307d7ba73794685399
Heljhum Enad <heljhumenad@gmail.com>
2020-01-14 16:53:59 +0800

    Add module for models

---
+++ parts/app/models/timestamp.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+class TimeStampModel(models.Model):
+    
+    class Meta:
+        abstract = True
+
+    created_at = models.DateTimeField(auto_now=True,
+                                      verbose_name=_("Created At")
+                                     )
+    
+    updated_at = models.DateTimeField(auto_now=True,
+                                     verbose_name=_("Updated At")
+                                     )
