d3de60a87884b296946f859c09dfa87132a518fc
Pr0digal <heljhumenad@gmail.com>
2020-02-17 16:07:36 +0800

    Alter database table name in model

--- parts/app/partsnumber/models.py
+++ parts/app/partsnumber/models.py
@@ -3,10 +3,11 @@ from django.utils.translation import gettext_lazy as _
 
 from parts.app.models.timestamp import TimeStampModel
 
+
 class PartsNumber(TimeStampModel):
 
     partnumber = models.CharField(max_length=200,
-                                  verbose_name = _("Parts Number"),
+                                  verbose_name=_("Parts Number"),
                                   )
 
     unit_measure = models.ForeignKey('UnitMeasure',
@@ -15,17 +16,29 @@ class PartsNumber(TimeStampModel):
 
     description = models.CharField(max_length=200,
                                    verbose_name=_("Description"),
-                                  )
+                                   )
+
+    class Meta:
+        db_table = _('partnumbers')
+        verbose_name = _("Part Number")
+        verbose_name_plural = _("Parts Number")
+        ordering = ['id']
 
     def __str__(self):
         return self.partnumber
-    
+
 
 class UnitMeasure(TimeStampModel):
 
-     um = models.CharField(max_length=20,
-                           verbose_name=_("Unit of Measure")
+    um = models.CharField(max_length=20,
+                          verbose_name=_("Unit of Measure")
                           )
 
-     def __str__(self):
-         return self.um
+    class Meta:
+        db_table = _("um")
+        verbose_name = _("Unit of Measure")
+        verbose_name_plural = _("Unit of Measures")
+        ordering = ['id']
+
+    def __str__(self):
+        return self.um
