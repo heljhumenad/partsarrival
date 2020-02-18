5b7e287dc1b4ba48bb61ffb9426f56bed0d7c2d2
Pr0digal <heljhumenad@gmail.com>
2020-02-18 11:00:23 +0800

    Fix urls typo for models

--- parts/app/views/item_class.py
+++ parts/app/views/item_class.py
@@ -1,9 +1,9 @@
 #  View Class for Partnumber and its description
-from django.url import reverse_lazy
+from django.urls import reverse_lazy
 from django.views import generic
 from django.utils.translation import ugettext_lazy as _
 
-from parts.app.partsnumber import PartNumberClass
+from parts.app.partsnumber.models import PartNumberClass
 
 
 class PartnumberClassTemplateView(generic.TemplateView):
