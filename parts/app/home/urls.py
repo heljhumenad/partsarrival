f0354dcd11ab99f86424e87bd0eed8360ae78304
Heljhum Enad <heljhumenad@gmail.com>
2020-02-24 11:30:03 +0800

    delete old url parsing using regexp

--- parts/app/home/urls.py
+++ parts/app/home/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path, include
 
 
+from parts.app.views.home import HomeTemplateView
+
 urlpatterns = [
      
+     path('', HomeTemplateView.as_view(),
+          name = 'home_template_view'
+         ),
 ]
