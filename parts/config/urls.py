7ceb4bf2188b7dbae7d0a22ffce081223b063c98
Heljhum Enad <heljhumenad@gmail.com>
2020-02-29 13:23:25 +0800

    Rollback from the last commit

--- parts/config/urls.py
+++ parts/config/urls.py
@@ -4,28 +4,11 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-
-    path('',
-         include('parts.app.home.urls')
-        ),
-
-    path('accounts/',
-         include('parts.app.accounts.urls')
-         ),
-
-    path('accounts/',
-         include('django.contrib.auth.urls')
-         ),
-
-    path('partsnumber/',
-         include('parts.app.partsnumber.urls')
-         ),
-
-    path('advisor/',
-         include('parts.app.advisor.urls')
-        ),
-        
-    path('__debug__', include(debug_toolbar.urls))
-
+    path("admin/", admin.site.urls),
+    path("", include("parts.app.home.urls")),
+    path("accounts/", include("parts.app.accounts.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("partsnumber/", include("parts.app.partsnumber.urls")),
+    path("advisor/", include("parts.app.advisor.urls")),
+    path("__debug__", include(debug_toolbar.urls)),
 ]
