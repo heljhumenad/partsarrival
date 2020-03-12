f7cba6fa8357045e5345973cffca597c41c0253a
Heljhum Enad <heljhumenad@gmail.com>
2020-03-12 10:41:41 +0800

    Test accounts mixins

---
+++ parts/app/tests/mixins/test_mixins.py
@@ -0,0 +1,20 @@
+from django.urls import reverse_lazy
+
+from parts.app.tests.base import TestCase
+from parts.app.mixins import useraccount_mixins
+
+
+class TestUserAccountMixins(TestCase):
+
+    def setUp(self):
+        super().setUp()
+        self._user_account_mixins = useraccount_mixins.UserAccountMixins
+
+    def tests_class_has_account_mixins_attr(self):
+        self.mixins = getattr(self._user_account_mixins, "login_url")
+        self.assertTrue(self.mixins)
+
+    def test_urls_for_accounts_mixins_redirect(self):
+        self.mixins_url = getattr(self._user_account_mixins, "login_url")
+        self.assertEqual(self.mixins_url, reverse_lazy(
+            'accounts:login'))
