11d8944627c961133f6e74a8973eff247e51cfa9
Heljhum Enad <heljhumenad@gmail.com>
2020-03-12 11:11:51 +0800

    Add tests

---
+++ parts/app/tests/advisor/test_views.py
@@ -0,0 +1,12 @@
+from parts.app.views import advisor
+from parts.app.tests.base import TestCase
+
+
+class TestAdvisor(TestCase):
+
+    def setUp(self):
+        super().setUp()
+        # self._advisor_views = advisor.__class__.__name__
+
+    def tests_advisor_views(self):
+        pass
