627440967e204c2c074f02e5eee8b1a1330d3be5
Heljhum Enad <heljhumenad@gmail.com>
2020-03-13 10:49:24 +0800

    Add to config dashboard url

--- parts/config/urls.py
+++ parts/config/urls.py
@@ -5,11 +5,20 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+
     path("", include("parts.app.home.urls")),
+
+    # path("dashboard/", include("parts.app.dashboard.urls")),
+
     path("accounts/", include("parts.app.accounts.urls")),
+
     path("accounts/", include("django.contrib.auth.urls")),
+
     path("partsnumber/", include("parts.app.partsnumber.urls")),
+
     path("advisor/", include("parts.app.advisor.urls")),
+
     path("arrival/", include("parts.app.arrival.urls")),
-    path("__debug__", include(debug_toolbar.urls)),
+
+    path("__debug__", include(debug_toolbar.urls)),  # Debug purposes
 ]
