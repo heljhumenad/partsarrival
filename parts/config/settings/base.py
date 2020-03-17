eae07bef8701d5db2d0549c1a3d6bed0e9f5c7a1
Heljhum Enad <heljhumenad@gmail.com>
2020-03-17 09:13:58 +0800

    Fix timezone not updated when do local time

--- parts/config/settings/base.py
+++ parts/config/settings/base.py
@@ -110,7 +110,9 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+# Based on the local timezone
+# https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+TIME_ZONE = "Asia/Manila"
 
 USE_I18N = True
 
