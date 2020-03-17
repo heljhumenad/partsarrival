9f015f8fe8ada3300a4452eee4018a452c97c9df
Heljhum Enad <heljhumenad@gmail.com>
2020-03-17 15:03:06 +0800

    Add message success mixins

--- parts/app/mixins/useraccount_mixins.py
+++ parts/app/mixins/useraccount_mixins.py
@@ -1,7 +1,21 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 
 
 class UserAccountMixins(LoginRequiredMixin):
     login_url = reverse_lazy("login")
     redirect_field_name = "next_link"
+
+
+class UserSuccessMessageMixin(LoginRequiredMixin, SuccessMessageMixin):
+    message = ''
+    success_message = _(
+        "%(customer_name)s is successfuly {0}!".format(message))
+
+    def get_success_message(self, cleaned_data, **kwargs):
+        return self.success_message % dict(
+            cleaned_data,
+            customer_name=self.object.customer_name,
+        )
--- parts/app/views/arrival.py
+++ parts/app/views/arrival.py
@@ -3,6 +3,7 @@ from django.urls import reverse_lazy
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+from parts.app.mixins.useraccount_mixins import UserSuccessMessageMixin
 from parts.app.arrival.models import PartsArrival
 from parts.app.forms.arrival_form import PartsArrivalForm
 
@@ -13,10 +14,11 @@ class PartsArrivalListView(LoginRequiredMixin, generic.ListView):
     paginate_by = 2
 
 
-class PartsArrivalCreateView(LoginRequiredMixin, generic.CreateView):
+class PartsArrivalCreateView(UserSuccessMessageMixin, generic.CreateView):
     template_name = 'arrival/add_arrival.html'
     form_class = PartsArrivalForm
     success_url = reverse_lazy('arrival:arrival_index')
+    # message = 'added'
 
     def get_context_data(self,  **kwargs):
         context = super(PartsArrivalCreateView,
@@ -24,17 +26,24 @@ class PartsArrivalCreateView(LoginRequiredMixin, generic.CreateView):
         context['arrival'] = PartsArrival.objects.all()
         return context
 
+    def get_success_message(self, cleaned_data):
+        return super(PartsArrivalCreateView, self).get_success_message(cleaned_data)
 
-class PartsArrivalUpdateView(LoginRequiredMixin, generic.UpdateView):
+
+class PartsArrivalUpdateView(UserSuccessMessageMixin, generic.UpdateView):
     template_name = 'arrival/add_arrival.html'
     form_class = PartsArrivalForm
     success_url = reverse_lazy('arrival:arrival_index')
+    # message = 'updated'
 
     def get_object(self, query_pk_and_slug=None):
         query = PartsArrival.objects.filter(
             id=self.kwargs['pk']
         ).first()
         return query
+
+    def get_success_message(self, cleaned_data):
+        return super(PartsArrivalUpdateView, self).get_success_message(cleaned_data)
 
 
 class PartsArrivalDetailView(LoginRequiredMixin, generic.DetailView):
