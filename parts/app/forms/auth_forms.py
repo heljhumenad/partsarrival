9bef3e1a598fac568f7ed48df1fee36244fda5fc
Heljhum Enad <heljhumenad@gmail.com>
2020-07-14 11:20:00 +0800

    Add authentication and registration forms

---
+++ parts/app/forms/auth_forms.py
@@ -0,0 +1,31 @@
+from django.contrib.auth import forms
+
+from parts.app.accounts.models import CustomUser
+
+
+class CustomUserCreationForm(forms.UserCreationForm):
+    class Meta(forms.UserCreationForm.Meta):
+        model = CustomUser
+        fields = forms.UserCreationForm.Meta.fields
+
+
+class CustomUserChangeForm(forms.UserChangeForm):
+    class Meta(forms.UserChangeForm):
+        model = CustomUser
+        # fields = UserChangeForm.Meta.fields
+        fields = ['username', 'first_name', 'last_name', 'email']
+
+
+class AccountPasswordResetForm(forms.PasswordResetForm):
+    class Meta(forms.PasswordResetForm):
+        model = CustomUser
+
+
+class AccountPasswordChangeForm(forms.PasswordChangeForm):
+    class Meta(forms.PasswordChangeForm):
+        model = CustomUser
+
+
+class AccountSetPasswordForm(forms.SetPasswordForm):
+    class Meta(forms.SetPasswordForm):
+        model = CustomUser
