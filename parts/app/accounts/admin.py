af0992f2d0125719a97f00817ddbcda98659d04f
Heljhum Enad <heljhumenad@gmail.com>
2020-07-14 11:23:33 +0800

    Change teh reverse url to custom user login url

--- parts/app/accounts/admin.py
+++ parts/app/accounts/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+# change this to avoid direct calling from main config
+from parts.config.settings import base
+
+# Register model
+from .models import CustomUser
+from parts.app.forms import auth_forms
+
+admin.site.site_header = base.ADMIN_SITE_HEADER
+admin.site.site_title = base.ADMIN_SITE_TITLE
+admin.site.index_title = base.ADMIN_SITE_TITLE
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = get_user_model
+    add_form = auth_forms.CustomUserCreationForm
+    form = auth_forms.CustomUserChangeForm
+    list_display = ['email', 'username']
+
+# admin.site.register(CustomUser, CustomUserAdmin)
