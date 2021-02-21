5e6c08011ce85bd0b112383dab18360a8425464e
Heljhum Enad <heljhumenad@gmail.com>
2021-02-21 22:17:53 +0800

    update the format of loading of  3rd party app

--- parts/models/managers/update_view.py
+++ parts/models/managers/update_view.py
@@ -6,6 +6,9 @@ class UpdateViewManager(models.Manager):
     def get_object(self, query_pk_and_slug=True):
         return super().get_object().filter(id=self.kwargs["pk"]).first()
 
+    def get_first_object(self):
+        return super().get_first_object().filter(id=self.kwargs["pk"]).first()
+
 
 class AbstractUpdateViewManager(models.Model):
     class Meta:
