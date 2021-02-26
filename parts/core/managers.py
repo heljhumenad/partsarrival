7695e6c41f30e338084aefa68a563fa8f9961274
Pr0digal <heljhumenad@gmail.com>
2021-02-26 14:16:39 +0800

    add managers and models to core app

---
+++ parts/core/managers.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+# Basic Hierarchy of classes and methods
+# - Custom ViewManager
+# - Abstract model Class
+class UpdateViewManager(models.Manager):
+    def get_object(self, query_pk_and_slug=True):
+        return super().get_object().filter(id=self.kwargs["pk"]).first()
+
+    def get_first_object(self, query_pk_and_slug=True):
+        return super().get_first_object().filter(id=self.kwargs["pk"]).first()
+
+
+class AbstractDescriptionModel(models.Model):
+
+    description = models.CharField(max_length=200, verbose_name=_("Description"))
+
+    class Meta:
+        abstract = True
+
+
+class AbstractUpdateViewManager(AbstractDescriptionModel, models.Model):
+    objects = UpdateViewManager()
+
+    class Meta:
+        abstract = True
