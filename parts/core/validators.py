7dc36b1921f17a8592d7be472af80efdcd238a62
Pr0digal <heljhumenad@gmail.com>
2021-03-01 14:04:11 +0800

    fix: change the validator base value

--- parts/core/validators.py
+++ parts/core/validators.py
@@ -1,8 +1,7 @@
-# Validators config 
+# Validators config
 
 # Configurations for Validation constant
-MAX_VALUE_OF_PARTNUMBER = 14
+MAX_VALUE_OF_PARTNUMBER = 11
 MIN_VALUE_OF_PARTNUMBER = 5
-DEFAULT_RO_RE_FORMAT = 'RO' or 'RE'
+DEFAULT_RO_RE_FORMAT = "RO" or "RE"
 DEFAULT_QTY = 1
-
