6c8645e6b725e78814a0dcb9a2fbff153a62a716
Pr0digal <heljhumenad@gmail.com>
2021-03-03 16:39:07 +0800

    fix: overflow of date range picker in canvas

--- parts/static/js/app.js
+++ parts/static/js/app.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   $('input[name="date_arrival"]').daterangepicker({
     singleDatePicker: true,
     timePicker: true,
+    drops: "up",
     locale: {
       format: "M/D/YYYY hh:mm A",
       cancelLabel: "Cancel"
