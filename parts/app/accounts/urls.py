18b79d1e32a52175efa1f0270fa69b4a879aa839
Pr0digal <heljhumenad@gmail.com>
2021-03-08 14:03:07 +0800

    feat: update urls for formatting

--- parts/app/accounts/urls.py
+++ parts/app/accounts/urls.py
@@ -4,24 +4,8 @@ from parts.app.views import accounts
 app_name = "accounts"
 
 urlpatterns = [
-    path(
-        "dashboard/",
-        accounts.AccountTemplateView.as_view(), 
-        name="account_view"
-    ),
-    path(
-        "login/",
-        accounts.AccountLoginView.as_view(),
-        name="login"
-    ),
-    path(
-        "logout/",
-        accounts.AccountLogoutView.as_view(),
-        name="logout_view"
-    ),
-    path(
-        "edit-user/<int:pk>",
-        accounts.AccountEditView.as_view(),
-        name="edit_user"
-    ),
+    path("dashboard", accounts.AccountTemplateView.as_view(), name="account_view"),
+    path("login", accounts.AccountLoginView.as_view(), name="login"),
+    path("logout", accounts.AccountLogoutView.as_view(), name="logout_view"),
+    path("update/user/<int:pk>", accounts.AccountEditView.as_view(), name="edit_user"),
 ]
