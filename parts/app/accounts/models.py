bf1f0209edb30e6e899883c0c78862fd97cd49c7
Pr0digal <heljhumenad@gmail.com>
2021-03-09 11:11:21 +0800

    feat: add profile for user

--- parts/app/accounts/models.py
+++ parts/app/accounts/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import AbstractUser, UserManager
 
-
+from parts.models.timestamp import TimeStampModel
 class CustomUserManager(UserManager):
     pass
 
@@ -16,3 +17,17 @@ class CustomUser(AbstractUser):
     @property
     def get_user_email(self):
         return self.email
+
+class ProfileUser(TimeStampModel, models.Model):
+
+    USER_LEVEL = (
+        ("1", "Manager"),
+        ("2", "Partsman"),
+        ("3", "Warehouseman")
+    )
+
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    user_level = models.CharField(max_length=200, verbose_name=_("User Level"), choices=USER_LEVEL)
+
+    def __str__(self):
+        return "{0} {1}".format(self.user, self.user_level)
