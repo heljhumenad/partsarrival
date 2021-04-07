fe8d1cab5645e682745a285573261cabd2e3b5b1
Heljhum Enad <heljhumenad@gmail.com>
2021-04-07 15:15:53 +0800

    update: transfer authorization mixin

--- parts/app/mixins/common_mixins.py
+++ parts/app/mixins/common_mixins.py
@@ -3,18 +3,6 @@ from django.utils.translation import ugettext_lazy as _
 
 from parts.core.mixins import MessageMixin
 
-class CheckUserAuthorizationMixin(AccessMixin):
-    # check proper authorizations for users
-    # throw 404, 303, 500
-    raise_exception = True
-
-    def dispatch(self, request, *args, **kwargs):
-        if not self.request.user.is_staff:
-            self.handle_no_permission()
-            return reverse_lazy('dashboard:dashboard_view_index')
-        return super(CheckUserAuthorizationMixin, self).dispatch(request, *args, **kwargs)
-
-
 class CustomerMessageMixin(MessageMixin):
     success_message = _(
         "%(field)s is successfuly %(messages)s")
