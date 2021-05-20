cce63dfe4877062b10816419f21262ea5d03effa
Heljhum Enad <heljhumenad@gmail.com>
2021-05-20 14:26:47 +0800

    update: ready for production on heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn parts.app.config.wsgi
---
+++ parts/config/settings/prod.py
@@ -0,0 +1,9 @@
+from .base import *
+
+
+DEBUG=False
+
+ALLOWED_HOST = ['parts-arrival.herokuapp.com', '127.0.0.1:8000']
+
+MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware',]
+
