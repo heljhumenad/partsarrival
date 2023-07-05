60d378d6a2933a091f03ada12dc2b4165b65ebbf
Heljhum Enad <heljhumenad@gmail.com>
2023-07-05 23:12:29 +0800

    :fire: code changes for to handle latest django release

--- parts/app/dashboard/apps.py
+++ parts/app/dashboard/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
+from parts.config.settings.base import DEFAULT_BASE_PATH
 
 
 class DashboardConfig(AppConfig):
-    name = 'dashboard'
+    name = DEFAULT_BASE_PATH + 'dashboard'
--- parts/app/partsnumber/apps.py
+++ parts/app/partsnumber/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
+from parts.config.settings.base import DEFAULT_BASE_PATH
 
 
 class PartsnumberConfig(AppConfig):
-    name = "partsnumber"
+    name = DEFAULT_BASE_PATH + "partsnumber"
