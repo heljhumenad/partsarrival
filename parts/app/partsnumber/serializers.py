228a4415eef57b930f5daeae9492aa3254c870a8
Heljhum Enad <heljhumenad@gmail.com>
2023-08-13 15:50:31 +0800

    :recyle: remove __fields__ in serializers to follow standard

--- parts/app/partsnumber/serializers.py
+++ parts/app/partsnumber/serializers.py
@@ -4,19 +4,47 @@ from parts.app.partsnumber.models import (
     PartsNumber,
     PartNumberClass,
     UnitMeasure,
+    SourceCode,
+    SellingPrice
 )
 
+# Serializer class for Partsnumber Model
 class PartNumberSerializer(serializers.ModelSerializer):
     class Meta:
         model = PartsNumber
-        fields = '__all__'
-        
+        fields = ['partnnumber', 'source_code', 'bar_code', 'selling_price',
+                  'status', 'unit_measure']
+
+
+# Parts Number Class Serializer
 class PartNumberClassSerializer(serializers.ModelSerializer):
     class Meta:
         model = PartNumberClass
-        fields = '__all__'
+        fields = ['class_name', 'charge_type']
 
+
+# UnitMeasure Class Serializer
 class UnitMeasureSerializer(serializers.ModelSerializer):
+
+    # TODO: update this if there's some field in the UnitMeasure model
+
     class Meta:
         model = UnitMeasure
-        fields = '__all__'
+        fields = ['sku_um']
+
+
+class SourceCodeSerialzer(serializers.ModelSerialzers):
+    class Meta:
+        model = SourceCode
+        fields = ['code', 'description']
+
+
+class SellingPriceSerializer(serializers.ModelSerialzer):
+   
+    # TODO: update this if there will be new fields in the selling price class 
+    # that is important for the partnumbers model
+
+    class Meta:
+        model = SellingPrice
+        fields = ['selling_price', 'discount_percentage']
+
